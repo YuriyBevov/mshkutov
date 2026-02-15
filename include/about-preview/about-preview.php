9539eb282c41fba37e695b776f9b1cb050567b08
Yuriy Bevov <yuriybevov@gmail.com>
2026-02-15 19:39:03 +0300

    добавил футер и блок о нас

--- include/about-preview/about-preview.php
+++ include/about-preview/about-preview.php
@@ -5,18 +5,18 @@ $APPLICATION->SetAdditionalCSS("/include/about-preview/about-preview.css");
 
 <section class="section about-preview">
   <div class="container">
-    <div class="grid">
+    <div class="grid animate-border">
       <div class="grid__item">
-        <h2>Обо мне</h2>
-        <h3>Шкутов Михаил <br><i>разработчик</i></h3>
+        <h2 class="strong-text">Обо мне</h2>
+        <h3 class="section__title">Шкутов Михаил <br><i>разработчик</i></h3>
       </div>
-      <div class="grid-item">
+      <div class="grid__item">
         <p>
           У вас разрозненные данные, ручные процессы продаж и нет прозрачной отчётности?
           Потерянные лиды, запоздалые письма и «ручные» рутинные задачи тормозят рост.
           Я превращаю CRM в рабочий инструмент, а не в цифровой архив.
         </p>
-        <a href="/about/">Подробнее</a>
+        <a href="/about/" class="main-link iconed">Подробнее</a>
       </div>
     </div>
   </div>
--- local/templates/mshkutov/footer.php
+++ local/templates/mshkutov/footer.php
@@ -1,8 +1,42 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 </main>
 <? if ($USER->isAdmin()): ?>
-  <footer>
-    FOOTER
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__row">
+        <? $APPLICATION->IncludeComponent(
+          "bitrix:menu",
+          "row-menu",
+          array(
+            "ALLOW_MULTI_SELECT" => "N",
+            "CHILD_MENU_TYPE" => "left",
+            "DELAY" => "N",
+            "MAX_LEVEL" => "1",
+            "MENU_CACHE_GET_VARS" => array(),
+            "MENU_CACHE_TIME" => "3600",
+            "MENU_CACHE_TYPE" => "N",
+            "MENU_CACHE_USE_GROUPS" => "Y",
+            "MENU_THEME" => "site",
+            "ROOT_MENU_TYPE" => "bottom",
+            "USE_EXT" => "Y",
+            "COMPONENT_TEMPLATE" => "row-menu"
+          ),
+          false
+        ); ?>
+      </div>
+      <div class="footer__row">
+        <a href="/" class="logo">
+          <img src="<?= SITE_TEMPLATE_PATH ?>/public/logo.svg" alt="Шкутов Михаил - Разработка IT-решений" width="146" height="55">
+        </a>
+        <span class="text">Copyright © 2026 All Rights Reserved.</span>
+        <a href="tel:+70000000000" class="main-link">+7 (000) 000-00-00</a>
+        <a href="mailto:email@mail.ru" class="main-link">email@mail.ru</a>
+
+        <div class="social">
+          social
+        </div>
+      </div>
+    </div>
   </footer>
 <? endif; ?>
 </body>
